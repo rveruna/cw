4ca90c2e2cfb81785f762d774fe68afa485103c9
Veronika <40212551@live.napier.ac.uk>
2016-11-22 18:27:09 +0000

    1

--- snakeeyes/app.py
+++ snakeeyes/app.py
@@ -10,6 +10,21 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     return render_template('home.html')
+@app.route('/test')
+def test():
+
+    POST_USERNAME = "python"
+    POST_PASSWORD = "python"
+
+    Session = sessionmaker(bind=engine)
+    s = Session()
+    query = s.query(User).filter(User.username.in_([POST_USERNAME]), User.password.in_([POST_PASSWORD]) )
+    result = query.first()
+    if result:
+        return "Object found"
+    else:
+        return "Object not found " + POST_USERNAME + " " + POST_PASSWORD
+
 
 @app.route('/signup')
 def signup():
