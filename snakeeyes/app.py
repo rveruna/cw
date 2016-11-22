1fe2c085543c0f2e04d167bfdf782e30bff9ac01
Veronika <40212551@live.napier.ac.uk>
2016-11-22 18:24:13 +0000

    1

--- snakeeyes/app.py
+++ snakeeyes/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask import Flask, flash, redirect, render_template, request, session, abort
 import os
-
+from sqlalchemy.orm import sessionmaker
+from tabledef import *
+engine = create_engine('sqlite:///tutorial.db', echo=True)
 
 app = Flask(__name__)
 
