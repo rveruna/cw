21c042e2e51b4839febf736bf7438947887dd84c
Veronika <40212551@live.napier.ac.uk>
2016-11-22 03:06:44 +0000

    form.py

--- snakeeyes/app.py
+++ snakeeyes/app.py
@@ -1,10 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('home.html')
 
+@app.route("/account/", methods=['POST','GET'])
+def account():
+  if request.method == 'POST':
+    print request.form
+    name = request.form['name']
+    return "Hello %s" % name
+
+  else:
+    page ='''
+    <html><body>
+      <form action="" method="post" name="form">
+        <label for="name">Name:</label>
+        <input type="text" name="name" id="name"/>
+        <input type="submit" name="submit" id="submit"/>
+      </form>
+      </body><html>'''
+
+    return page
 
 @app.route('/terms')
 def terms():
