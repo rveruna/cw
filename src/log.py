d2cba37e55aaa52cdaa23e29d44f5453af435658
Veronika <40212551@live.napier.ac.uk>
2016-11-30 21:32:06 +0000

    kl

--- src/log.py
+++ src/log.py
@@ -2,11 +2,11 @@ from flask import Flask
 from logging import FileHandler, WARNING
 
 app = Flask(__name__)
+if not app.debug:
+    file_handler = FileHandler('errorlog.txt')
+    file_handler.setLevel(WARNING)
 
-file_handler = FileHandler('errorlog.txt')
-file_handler.setLevel(WARNING)
-
-app.logger.addHandler(file_handler)
+    app.logger.addHandler(file_handler)
 
 @app.route('/')
 def index():
