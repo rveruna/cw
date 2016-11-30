77e3c018c58afa1a564ef887f7542ef9836e9922
Veronika <40212551@live.napier.ac.uk>
2016-11-30 21:26:52 +0000

    d

---
+++ src/log.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from logging import FileHandler, WARNING
+
+app = Flask(__name__)
+
+file_handler = FileHandler('errorlog.txt')
+file_handler.setLevel(WARNING)
+
+app.logger.addHandler(file_handler)
+
+@app.route('/')
+def index():
+    return 1 / 0
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
