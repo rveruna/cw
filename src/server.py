52163726b7180f1c3d66238f08fb824cea6b4087
Veronika <40212551@live.napier.ac.uk>
2016-11-30 21:05:45 +0000

    add

--- src/server.py
+++ src/server.py
@@ -16,4 +16,4 @@ if __name__ == '__main__':
     handler = RotatingFileHandler('foo.log', maxBytes=10000, backupCount=1)
     handler.setLevel(logging.INFO)
     app.logger.addHandler(handler)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
